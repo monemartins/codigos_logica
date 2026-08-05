8ca883cdfb67f2adb63a4e70779ecf2bb47a3877
Si Martins <161902396+monemartins@users.noreply.github.com>
2026-08-05 20:38:24 -0300

    Finish

---
+++ dict.01.py
@@ -0,0 +1,18 @@
+produtos[ 
+    {"nome" : "Vestido",
+    "codigo" : "01",
+    "preco" : "100.00",
+    "quantidade" : "10",
+    "categoria" : "Roupas",},
+    
+    {"nome" : "Blusa",
+    "codigo" : "02",
+    "preco" : "55.00",
+    "quantidade" : "25",
+    "categoria" : "Roupas"},
+    
+    {"nome" : "Calca",
+    "codigo" : "03",
+    "preco" : ".00",
+    "quantidade" : "10",
+    "categoria" : "Roupas",}]
