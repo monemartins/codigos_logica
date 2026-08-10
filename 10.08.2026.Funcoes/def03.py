798c3942b6f76f6705aab3f9b0b42ce1f1f2783d
Si Martins <161902396+monemartins@users.noreply.github.com>
2026-08-10 19:51:43 -0300

    Atividades

---
+++ 10.08.2026.Funcoes/def03.py
@@ -0,0 +1,8 @@
+def fahrenheit_to_celsius(fahrenheit):
+  c = (fahrenheit - 32) * 5 / 9
+  return c
+  print("Acabou")
+
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(95))
+print(fahrenheit_to_celsius(50))
