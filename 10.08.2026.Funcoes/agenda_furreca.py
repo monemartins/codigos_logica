8c04b9b941c2ef68bdeac7a737ba5efc31fe66a8
Si Martins <161902396+monemartins@users.noreply.github.com>
2026-08-10 21:33:56 -0300

    Teste Agenda

---
+++ 10.08.2026.Funcoes/agenda_furreca.py
@@ -0,0 +1,106 @@
+############################################
+# 2026.08.10.Funcoes\agenda_furreca.py     #
+# AGENDA FURRECA.PY                        #
+# Versão 2026.08.10                        #
+# By Luferat - https://github.xonm/Luferat #
+############################################
+
+import subprocess
+import random
+
+# numero = random.randint(1, 1000)
+
+# Baco de dados
+database = {
+    "1": { "name": "Joca da Silva", "contact": "(21) 998877665"},
+    "120": { "name": "Mariana Sirilampo", "contact": "mariana@sirilampo.com.br"}
+}
+
+def cls():
+    subprocess.run("cls", shell=True)
+
+def new_contact():
+    cls()
+    print("[ AGENDA FURRECA - NOVO CONTATO ]")
+    
+    print("\nDigite os dados do contato:\n")
+
+    name = input(" • Nome: ")
+    contact = input(" • Contato: ")
+    key = str(random.randint(1, 1000))
+
+    database[key] = dict(name = name, contact = contact)
+
+    print(f"\nUsuário com ID {key} adicionado!")
+    input("Tecle [Enter] para continuar")
+    main()
+
+def list_contacts():
+    cls()
+    print("[ AGENDA FURRECA - LISTA CONTATOS ]")
+
+    # ...
+    
+    input("Tecle [Enter] para continuar")
+    main()
+
+def edit_contact():
+    cls()
+    print("[ AGENDA FURRECA - EDITA CONTATO ]")
+
+    # ...
+    
+    input("Tecle [Enter] para continuar")
+    main()
+
+def delete_contact():
+    cls()
+    print("[ AGENDA FURRECA - APAGA CONTATO ]")
+
+    # ...
+    
+    input("Tecle [Enter] para continuar")
+    main()
+
+# Programa principal
+def main(erro = str()):
+    # Main loop
+    while True:
+        cls() 
+        print("[ AGENDA FURRECA - MENU PRINCIPAL ]")
+        if erro:
+            print(erro)
+        erro = str()
+        # Debug
+        print(database)
+        print('''
+Opções:
+
+1 - Novo contato
+2 - Listar contatos
+3 - Editar contato
+4 - Apagar contato
+0 - Sair do programa
+    ''')
+        
+        opcao = input("Escolha uma opção: ")
+
+        match opcao:
+            case "1":
+                new_contact()
+            case "2":
+                list_contacts()
+            case "3":
+                edit_contact()
+            case "4":
+                delete_contact()
+            case "0":
+                cls()
+                print("\nAcabou!")
+                exit()
+            case _:
+                erro = "Digite uma opção válida!"
+                main(erro)
+
+# "Roda" o programa
+main()     
--- 10.08.2026.Funcoes/def01.py
+++ 10.08.2026.Funcoes/def01.py
@@ -2,7 +2,8 @@ def my_function():
   print("Hello from a function")
   a = 20
   if 10 == a:
- else:
+    print("É igual")
+  else:
     print("É diferente")
 
 my_function()
